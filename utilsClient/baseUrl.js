8393e2bfcd130c74c91b874ee48f8d8b98990919
kshivam99 <sawarn101@gmail.com>
2021-06-15 12:39:03 +0530

    baseUrl

--- utilsClient/baseUrl.js
+++ utilsClient/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://matrixweb.vercel.app/";
+    : window.location.origin;
 
 module.exports = baseUrl;
