04c355acf6922ba184ebcf0f04d2f0eae96012d4
kshivam99 <sawarn101@gmail.com>
2021-06-15 12:49:22 +0530

    baseUrl

--- utilsClient/baseUrl.js
+++ utilsClient/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://matrixwebsocial.herokuapp.com/";
+    : "https://matrixwebsocial.herokuapp.com";
 
 module.exports = baseUrl;
